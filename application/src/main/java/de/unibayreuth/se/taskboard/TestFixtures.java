2ee729b8c2b08dc5169e87c6dec3990b7b243f4b
Benedikt Rief <rief.benedikt@gmail.com>
2025-02-20 15:07:49 +0100

    Change loading of test data and redudundant assignment of status

--- application/src/main/java/de/unibayreuth/se/taskboard/TestFixtures.java
+++ application/src/main/java/de/unibayreuth/se/taskboard/TestFixtures.java
@@ -18,15 +18,15 @@ public class TestFixtures {
     );
 
     private static final List<Task> TASKS = List.of(
-            generateTask("Task 1", "Description 1", TaskStatus.TODO),
-            generateTask("Task 2", "Description 2", TaskStatus.TODO),
-            generateTask("Task 3", "Description 3", TaskStatus.TODO),
-            generateTask("Task 4", "Description 4", TaskStatus.DOING),
-            generateTask("Task 5", "Description 5", TaskStatus.DOING),
-            generateTask("Task 6", "Description 6", TaskStatus.DOING),
-            generateTask("Task 7", "Description 7", TaskStatus.DONE),
-            generateTask("Task 8", "Description 8", TaskStatus.DONE),
-            generateTask("Task 9", "Description 9", TaskStatus.DONE)
+            new Task("Task 1", "Description 1"),
+            new Task("Task 2", "Description 2"),
+            new Task("Task 3", "Description 3"),
+            new Task("Task 4", "Description 4"),
+            new Task("Task 5", "Description 5"),
+            new Task("Task 6", "Description 6"),
+            new Task("Task 7", "Description 7"),
+            new Task("Task 8", "Description 8"),
+            new Task("Task 9", "Description 9")
     );
 
     public static List<User> getUsers() {
@@ -53,11 +53,5 @@ public class TestFixtures {
                 .map(SerializationUtils::clone) // prevent issues when tests modify the fixture objects
                 .map(taskService::create)
                 .collect(Collectors.toList());
-    }
-
-    private static Task generateTask(String title, String description, TaskStatus status) {
-        Task task = new Task(title, description);
-        task.setStatus(status);
-        return task;
     }
 }
